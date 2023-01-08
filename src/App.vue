fdeab5aca11d93d57d4d5330a393f54a73a64d0e
Benjamin Wilhelm <logins@b-wilhelm.de>
2023-01-08 16:36:40 +0100

    WIP

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import MarkdownEditor from "./components/MarkdownEditor.vue";
 import FileTree from "./components/FileTree.vue";
+import GoogleDrive from "./components/GoogleDrive.vue";
 </script>
 
 <template>
   <main>
-    <FileTree />
-    <MarkdownEditor />
+    <!-- <FileTree />
+    <MarkdownEditor /> -->
+    <GoogleDrive />
   </main>
 </template>
---
+++ src/components/GoogleDrive.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { loadScript } from "vue-plugin-load-script";
+
+</script>
+
+<template>
+    <div>
+        Hello World
+    </div>
+</template>
